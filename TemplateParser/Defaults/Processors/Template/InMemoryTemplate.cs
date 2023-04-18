db415bd8b31b12dcda3b50ebe504d0a9be4e3415
romesh-abeyawardena <romesh.abeyawardena@bellrockgroup.co.uk>
2023-04-18 16:12:14 +0100

    Added InMemory tempalte

---
+++ TemplateParser/Defaults/Processors/Template/InMemoryTemplate.cs
@@ -0,0 +1,16 @@
+﻿
+using TemplateParser.Contracts;
+
+namespace TemplateParser.Defaults.Processors.Template;
+
+public class InMemoryTemplate : BaseTemplateProcessor
+{
+    public InMemoryTemplate() : base(TemplateType.InMemoryTemplate)
+    {
+    }
+
+    public override Task Process(ITemplate template, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
