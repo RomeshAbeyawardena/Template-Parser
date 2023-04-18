64376f5d4deb106f097f9387c68592d8223c8231
romesh-abeyawardena <romesh.abeyawardena@bellrockgroup.co.uk>
2023-04-18 16:20:48 +0100

    Implemented template executor

--- TemplateParser/Contracts/ITemplateExecutor.cs
+++ TemplateParser/Contracts/ITemplateExecutor.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TemplateParser.Contracts
+﻿namespace TemplateParser.Contracts
 {
-    internal interface ITemplateExecutor
+    public interface ITemplateExecutor
     {
+        Task Execute(IEnumerable<ITemplate> templates, CancellationToken cancellationToken);
     }
 }
---
+++ TemplateParser/Defaults/DefaultTemplateExecutor.cs
@@ -0,0 +1,25 @@
+﻿using TemplateParser.Contracts;
+
+namespace TemplateParser.Defaults;
+
+public class DefaultTemplateExecutor : ITemplateExecutor
+{
+    private readonly IEnumerable<ITemplateProcessor> templateProcessors;
+
+    public DefaultTemplateExecutor(IEnumerable<ITemplateProcessor> templateProcessors)
+    {
+        this.templateProcessors = templateProcessors;
+    }
+
+    public async Task Execute(IEnumerable<ITemplate> templates, CancellationToken cancellationToken)
+    {
+        foreach(var template in templates) 
+        { 
+            var processors = templateProcessors.Where(t => t.CanProcess(template));
+            foreach(var processor in processors)
+            {
+                await processor.Process(template, cancellationToken);
+            }
+        }
+    }
+}
